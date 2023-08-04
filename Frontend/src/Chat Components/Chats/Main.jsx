ae90712096190a316fc259fac69cd92335a56762
santhoshparthiban <santhoshparthiban2002@gmail.com>
2023-08-04 19:14:15 +0530

    initiator Page

--- Frontend/src/Chat Components/Chats/Main.jsx
+++ Frontend/src/Chat Components/Chats/Main.jsx
@@ -1,32 +1,26 @@
+import Profilebar from "../Profilebar";
+import Headers from "../Title";
 
-
-import Profilebar from '../Profilebar';
-import Headers from '../Title';
-
-import React from 'react';
+import React from "react";
 import { Outlet, Link } from "react-router-dom";
-import '../../css/Main.css';
+import "../../css/Main.css";
 
-const Main = ()=>{
-    return(
-<> 
-<div className='d-lg-flex d-none flex-column'>
- <div><Headers /></div> 
- <div className='d-flex ' >
- <Profilebar />
- <div className='d-flex w-100 widget'>
-  <Outlet />
+const Main = () => {
+  return (
+    <>
+      <div className="d-flex  flex-column">
+        <div>
+          <Headers />
+        </div>
+        <div className="d-flex ">
+          <Profilebar />
+          <div className="d-flex w-100 widget">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-</div>
-</div>
-                 
-            </div>
-            <div><h1 className='d-flex d-lg-none justify-content-center align-items-center flex-column'>Open on Computer</h1></div>
-
-  </>  
-  
-  
-  )
-}  
-
-export default Main
+export default Main;
